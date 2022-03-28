261a6141f2ddefbb0b9827b5e815c2d397fc0e39
Ben Caunt <bdcaunt@gmail.com>
2022-03-28 12:24:12 -0400

    Add docstrings to motion profile stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AsymmetricProfile/MotionConstraint.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AsymmetricProfile/MotionConstraint.java
@@ -1,9 +1,26 @@
 package org.firstinspires.ftc.teamcode.AsymmetricProfile;
 
+
+/**
+ * This is a constraint on our motion profile, it describes the dynamics we want our robot to
+ * exert over a given trajectory
+ */
 public class MotionConstraint {
 
+    /**
+     * the maximum initial acceleration we allow the robot to experience
+     */
     public double max_acceleration;
+    /**
+     * the maximum final deceleration we allow the robot to experience
+     *
+     * Generally this is lower than {@link #max_acceleration}
+     */
     public double max_deceleration;
+
+    /**
+     * the maximum velocity we can reasonably reach in this given trajectory
+     */
     public double max_velocity;
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AsymmetricProfile/MotionState.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AsymmetricProfile/MotionState.java
@@ -1,8 +1,22 @@
 package org.firstinspires.ftc.teamcode.AsymmetricProfile;
 
+
+/**
+ * the current state of the robot at a given point in time as calculated
+ * by {@link AsymmetricMotionProfile}
+ */
 public class MotionState {
+    /**
+     * current position
+     */
     protected double x;
+    /**
+     * current velocity
+     */
     protected double v;
+    /**
+     * current acceleration
+     */
     protected double a;
 
     /**
@@ -17,14 +31,25 @@ public class MotionState {
         this.a = a;
     }
 
+    /**
+     * get the current acceleration
+     * @return scalar for acceleration
+     */
     public double getA() {
         return a;
     }
-
+    /**
+     * get the current velocity
+     * @return scalar for velocity
+     */
     public double getV() {
         return v;
     }
 
+    /**
+     * get the current position
+     * @return scalar for position
+     */
     public double getX() {
         return x;
     }
